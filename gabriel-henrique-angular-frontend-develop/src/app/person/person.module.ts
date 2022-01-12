77bf799663efac833955b49d0ee6f429b4358ff1
Gabriel Henrique <Gabriel.Henrique@senior.com.br>
2022-01-12 17:13:31 -0300

    view

--- gabriel-henrique-angular-frontend-develop/src/app/person/person.module.ts
+++ gabriel-henrique-angular-frontend-develop/src/app/person/person.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { ViewComponent } from './view/view.component';
 
 
 
 @NgModule({
-  declarations: [ListComponent, CadastroComponent],
+  declarations: [ListComponent, CadastroComponent, ViewComponent],
   imports: [
     CommonModule
   ],
-  exports: [ListComponent, CadastroComponent]
+  exports: [ViewComponent]
 
 })
 export class PersonModule { }
